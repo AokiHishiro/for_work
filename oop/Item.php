013f5bd8f948928815356ffa19528ca9ac508b68
Alex Nasuta <54862750+AokiHishiro@users.noreply.github.com>
2021-12-13 15:11:39 +0300

    Update Item.php

--- oop/Item.php
+++ oop/Item.php
@@ -1,39 +1,48 @@
-
 <?php
 
-    final class Item {
-        private int $id ;
-        private bool $changed;
-        private string $name;
-        private int $status;
+final class Item {
+    private int $id ;
+    private bool $changed;
+    private string $name;
+    private int $status;
 
-        function __construct($id) {
-          $this->id = $id;
+    function __construct($id) {
+        $this->id = $id;
+        require_once '../config/conection.php';//запускается в самом начале(подклбчение прописывается в отдельном файле)
+    }
 
-        }
+    public function __set($name,$value){//срабатывает, когда мы пытается установить значения вполе класса к которому невозможно получить доступ
 
-        private function init(){
+        if($name == '$id') {
+            echo "nelzya";
+        }elseif(is_string($name =='$name' )||is_int($value=='$status')){
+            $this->$name=$value;}
+        elseif(empty($value)||empty($name)){
+            echo "zapolnite";}
+    }
 
-       
-        }
 
-        public function save(){
+    public function __get($name){//считывает значение поля к которому нет доступа
+        return $this->$name;
+    }
 
-        }
+
+    private function init($connect,$name,$status){// передает $name и  $status для записи| функция получает значение столбцов в свойства
+
+    $name =mysqli_query($connect, "SELECT name FROM 'objects'");
+    $status = mysqli_query($connect, "SELECT status FROM 'objects'");
+    $name= mysqli_fetch_all($name);
+    $status= mysqli_fetch_all($status);
 
     }
 
-    $Item = new Item();
+    public function save($connect,$name,$status,$id){// позволяет добавлять новое  в базу данных в случае, если были внесены изменения извне
+      //  mysqli_query($connect,"UPDATE 'objects' SET 'name' = '$name','status' = $status WHERE  'objects'.'Id'=$id" ); ->должно быть отдельной функцией (для вноса изменений)
+        mysqli_query($connect,"INSERT INTO 'objects'['id','name','status'], VALUES (NULL,'$name','$status')");//для заполнения таблицы извне
 
-function func(event) {
-    if(event.keyCode == '38'){
-        console.log('Нажата клавиша вверх');
     }
 
-    if(event.keyCode == '40'){
-        console.log('нажата клавиша вниз');
-    }
 }
 
-window.addEventListener('keydown', func, false);
+
 ?>
